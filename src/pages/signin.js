1ed4f8e264f029c83465319c017df10d6d64a0b8
zestairlove <violetdessin@gmail.com>
2020-10-05 01:39:16 +0900

    add firebase top level authentication

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   // check form input elements are valid email & password
   const isInvalid = password === '' || emailAddress === '';
+
   const handleSignIn = (e) => {
     e.preventDefault();
 
     // firebase work here!
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        // push to the browse page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
   };
 
   return (
