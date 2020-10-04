f77395c3dbfef3b96b75649ee74c0b48982b3549
zestairlove <violetdessin@gmail.com>
2020-10-05 07:00:36 +0900

    add header, profile

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
-  return <p>Hello from Browse</p>;
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
+  const slides = selectionFilter({ series, films });
+  // pass it to the browse container
+
+  return <BrowseContainer slides={slides}>Hello from Browse</BrowseContainer>;
 }
